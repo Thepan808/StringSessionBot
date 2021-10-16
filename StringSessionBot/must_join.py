9df049edda856c2f6fc53d723e838a352bec5113
Thepan808 <89053669+Thepan808@users.noreply.github.com>
2021-10-16 03:27:07 -0300

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -19,10 +19,10 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"♦️ Você mesmo! Entre no canal [Entra aí]({link}) para me iniciar. E obrigado ♦️ !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton("♦️ Entre no canal ♦️", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
